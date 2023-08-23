0c31d3b63131262a766dae670dfba2631c5364ae
gaus07 <gausnoori07@gmail.com>
2023-08-23 17:39:19 +0530

    buildspec

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+apt install -y nginx
 apt install -y docker.io
 apt install -y docker-compose
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t cart:v1 /app                            
-docker run -d -p 3000:3000 cart:v1                           
+docker build -t cart:v1 /app
+docker run -d -p 3000:3000 cart:v1
